bc346c8ecb6b4123a9d44ac14f2dc45f08e86c0d
viniciushrk <viniciusrosa444@gmail.com>
2019-02-18 11:19:41 -0300

    primeirto commit do projeto

---
+++ app/src/main/java/com/example/viniciushrk/sipam/segundaTela.java
@@ -0,0 +1,16 @@
+package com.example.viniciushrk.sipam;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by viniciushrk on 18/02/2019.
+ */
+
+public class segundaTela extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.segundatela);
+    }
+}
